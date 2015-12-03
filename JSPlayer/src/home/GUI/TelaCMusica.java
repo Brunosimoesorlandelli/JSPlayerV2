3b30b51b13541450efe7ef35dd9e5d0b76dc57fa
FlyZubs <mateusfitti@gmail.com>
2015-12-02 23:46:53 -0300

    FIM2

--- JSPlayer/src/home/GUI/TelaCMusica.java
+++ JSPlayer/src/home/GUI/TelaCMusica.java
@@ -1,175 +1,209 @@
 package home.GUI;
 
-import java.awt.Toolkit;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.File;
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
 
-import javax.swing.JButton;
-import javax.swing.JFileChooser;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.JSpinner;
-import javax.swing.JTextField;
-import javax.swing.SpinnerNumberModel;
 import javax.swing.border.EmptyBorder;
-import javax.swing.filechooser.FileFilter;
-import javax.swing.filechooser.FileNameExtensionFilter;
 
 import home.negocio.Fachada;
 import home.negocio.IFachada;
-import home.negocio.beans.Musica;
 import home.negocio.beans.Usuario;
 
-public class TelaCMusica extends JFrame {
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.awt.event.ActionEvent;
+import javax.swing.ImageIcon;
+import java.awt.Toolkit;
+import java.awt.Color;
+import java.awt.Font;
+import javax.swing.JSpinner;
+import javax.swing.SpinnerNumberModel;
+import javax.swing.SpinnerDateModel;
+import java.util.Date;
+import java.util.Calendar;
+import java.awt.SystemColor;
+
+public class TelaCadastro extends JFrame {
 
 	private JPanel contentPane;
-	private JTextField textTitulo;
-	private JTextField textArtista;
-	private JTextField textGenero;
-	private JTextField textAlbum;
-	private JTextField textEndereco;
-	private String end;
+	private JTextField textCidade;
+	private JTextField textEmail;
+	private JTextField textNome;
+	private JTextField textCEmail;
 
-	/**
-	 * Launch the application.
-	 */
-
-	/**
-	 * Create the frame.
-	 */
-	public TelaCMusica(Usuario u) {
+	
+	public TelaCadastro() {
 		IFachada f = Fachada.getInstance();
-		end = null;
 
+		setIconImage(Toolkit.getDefaultToolkit().getImage("Imagens\\f39bfcb5.png"));
 		setTitle("JSPlayer");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setIconImage(Toolkit.getDefaultToolkit().getImage("Imagens\\f39bfcb5.png"));
 		setBounds(100, 100, 850, 528);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 
-		JLabel lblNomeDaMsica = new JLabel("Titulo:");
-		lblNomeDaMsica.setBounds(12, 13, 103, 26);
-		contentPane.add(lblNomeDaMsica);
+		JSpinner spinIdade = new JSpinner();
+		spinIdade.setToolTipText("");
+		spinIdade.setModel(new SpinnerNumberModel(new Integer(13), new Integer(13), null, new Integer(1)));
+		spinIdade.setBounds(140, 307, 97, 29);
+		contentPane.add(spinIdade);
 
-		JLabel lblArtista = new JLabel("Artista:");
-		lblArtista.setBounds(12, 52, 56, 16);
-		contentPane.add(lblArtista);
+		JLabel lblConfirmaoDeEmail = new JLabel("Confirma\u00E7\u00E3o de Email: ");
+		lblConfirmaoDeEmail.setFont(new Font("OCR A Extended", Font.PLAIN, 14));
+		lblConfirmaoDeEmail.setForeground(SystemColor.controlHighlight);
+		lblConfirmaoDeEmail.setBounds(10, 136, 227, 37);
+		contentPane.add(lblConfirmaoDeEmail);
 
-		JLabel lbllbum = new JLabel("Album:");
-		lbllbum.setBounds(12, 91, 56, 16);
-		contentPane.add(lbllbum);
+		JLabel lblNomeDoUsuario = new JLabel("Nome do Usuario: ");
+		lblNomeDoUsuario.setFont(new Font("OCR A Extended", Font.PLAIN, 14));
+		lblNomeDoUsuario.setForeground(SystemColor.controlHighlight);
+		lblNomeDoUsuario.setBounds(10, 35, 188, 14);
+		contentPane.add(lblNomeDoUsuario);
 
-		JLabel lblAno = new JLabel("Ano:");
-		lblAno.setBounds(12, 126, 56, 16);
-		contentPane.add(lblAno);
+		JLabel lblEmail = new JLabel("Email: ");
+		lblEmail.setFont(new Font("OCR A Extended", Font.PLAIN, 14));
+		lblEmail.setForeground(SystemColor.controlHighlight);
+		lblEmail.setBounds(10, 91, 188, 14);
+		contentPane.add(lblEmail);
 
-		JLabel lblGnero = new JLabel("Genero:");
-		lblGnero.setBounds(12, 155, 56, 16);
-		contentPane.add(lblGnero);
+		JLabel lblLocalizao = new JLabel("Cidade: ");
+		lblLocalizao.setFont(new Font("OCR A Extended", Font.PLAIN, 14));
+		lblLocalizao.setForeground(SystemColor.controlHighlight);
+		lblLocalizao.setBounds(10, 203, 188, 14);
+		contentPane.add(lblLocalizao);
 
-		textTitulo = new JTextField();
-		textTitulo.setBounds(142, 15, 158, 22);
-		contentPane.add(textTitulo);
-		textTitulo.setColumns(10);
+		JLabel lblEstado = new JLabel("Estado: ");
+		lblEstado.setFont(new Font("OCR A Extended", Font.PLAIN, 14));
+		lblEstado.setForeground(SystemColor.controlHighlight);
+		lblEstado.setBounds(447, 203, 108, 14);
+		contentPane.add(lblEstado);
 
-		textArtista = new JTextField();
-		textArtista.setBounds(142, 49, 158, 22);
-		contentPane.add(textArtista);
-		textArtista.setColumns(10);
+		JLabel lblSexo = new JLabel("Sexo: ");
+		lblSexo.setFont(new Font("OCR A Extended", Font.PLAIN, 14));
+		lblSexo.setForeground(SystemColor.controlHighlight);
+		lblSexo.setBounds(10, 257, 97, 14);
+		contentPane.add(lblSexo);
 
-		textAlbum = new JTextField();
-		textAlbum.setBounds(142, 88, 158, 22);
-		contentPane.add(textAlbum);
-		textAlbum.setColumns(10);
+		JLabel lblIdade = new JLabel("Idade: ");
+		lblIdade.setFont(new Font("OCR A Extended", Font.PLAIN, 14));
+		lblIdade.setForeground(SystemColor.controlHighlight);
+		lblIdade.setBounds(10, 314, 97, 14);
+		contentPane.add(lblIdade);
 
-		JSpinner spinAno = new JSpinner();
-		spinAno.setModel(new SpinnerNumberModel(new Integer(1900), new Integer(1500), null, new Integer(1)));
-		spinAno.setBounds(142, 123, 97, 22);
-		contentPane.add(spinAno);
+		JComboBox comboSexo = new JComboBox();
+		comboSexo.setFont(new Font("OCR A Extended", Font.PLAIN, 14));
+		comboSexo.setToolTipText("");
+		comboSexo.setModel(new DefaultComboBoxModel(new String[] { "<Selecione>", "Masculino", "Feminino" }));
+		comboSexo.setBounds(180, 250, 132, 29);
+		contentPane.add(comboSexo);
 
-		textGenero = new JTextField();
-		textGenero.setBounds(142, 158, 158, 22);
-		contentPane.add(textGenero);
-		textGenero.setColumns(10);
-		
-		textEndereco = new JTextField();
-		textEndereco.setBounds(115, 266, 258, 22);
-		contentPane.add(textEndereco);
-		textEndereco.setColumns(10);
+		JComboBox comboEstado = new JComboBox();
+		comboEstado.setFont(new Font("OCR A Extended", Font.PLAIN, 14));
+		comboEstado.setModel(new DefaultComboBoxModel(
+				new String[] { "<Selecione>", "AC\t ", "AL\t ", "AP\t ", "AM\t ", "BA\t ", "CE\t ", "DF\t ", "ES\t ",
+						"GO\t ", "MA\t ", "MT\t ", "MS\t ", "MG\t ", "PA\t ", "PB\t ", "PR\t ", "PE\t ", "PI\t ",
+						"RJ\t ", "RN\t ", "RS\t ", "RO\t ", "RR\t ", "SC\t ", "SP\t ", "SE\t ", "TO" }));
+		comboEstado.setBounds(565, 197, 132, 29);
+		contentPane.add(comboEstado);
 
-		JButton btnCadastrar = new JButton("Confirmar");
-		btnCadastrar.addActionListener(new ActionListener() {
+		textCEmail = new JTextField();
+		textCEmail.setBounds(247, 140, 195, 29);
+		contentPane.add(textCEmail);
+		textCEmail.setColumns(10);
 
-			@Override
+		textCidade = new JTextField();
+		textCidade.setBounds(247, 196, 190, 29);
+		contentPane.add(textCidade);
+		textCidade.setColumns(10);
+
+		textEmail = new JTextField();
+		textEmail.setBounds(247, 84, 195, 29);
+		contentPane.add(textEmail);
+		textEmail.setColumns(10);
+
+		textNome = new JTextField();
+		textNome.setBounds(247, 28, 195, 28);
+		contentPane.add(textNome);
+		textNome.setColumns(10);
+
+		JButton btnNewButton = new JButton("Confirmar");
+		btnNewButton.setFont(new Font("OCR A Extended", Font.PLAIN, 14));
+		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				if (textTitulo.getText().length() > 0) {
-					if (textArtista.getText().length() > 0) {
-						if (textAlbum.getText().length() > 0) {
-							if (textGenero.getText().length() > 0) {
-								f.cadastrarMusica(new Musica(textTitulo.getText(), textArtista.getText(),
-										textAlbum.getText(), spinAno.getValue(), textGenero.getText(), end));
-								dispose();
-								TelaUsuario telaUsuario = new TelaUsuario(u);
-								telaUsuario.setVisible(true);
-								telaUsuario.setResizable(false);
-								telaUsuario.setLocationRelativeTo(null);
+				if (textNome.getText().length() > 0) {
+					if (textEmail.getText().contains("@") && textEmail.getText().length() > 0
+							&& textEmail.getText().equals(textCEmail.getText())) {
+						if (textCidade.getText().length() > 0) {
+							if (!comboEstado.getSelectedItem().equals("<Selecione>")) {
+								if (comboSexo.getSelectedItem().equals("Masculino")
+										|| comboSexo.getSelectedItem().equals("Feminino")) {
+									if (f.cadastrarUsuario(new Usuario(textNome.getText(), textEmail.getText(),
+											textCidade.getText(), comboEstado.getSelectedItem(),
+											comboSexo.getSelectedItem(), spinIdade.getValue()))) {
+										dispose();
+										TelaLogin telaLogin = new TelaLogin();
+										telaLogin.setVisible(true);
+										telaLogin.setLocationRelativeTo(null);
+										telaLogin.setResizable(false);
+									}
+								} else {
+									JOptionPane.showMessageDialog(null, "ERRO, SEXO INVALIDO");
+								}
 							} else {
-								JOptionPane.showMessageDialog(null, "ERRO, GENERO INVALIDO");
+								JOptionPane.showMessageDialog(null, "ERRO, ESTADO INVALIDO");
 							}
 						} else {
-							JOptionPane.showMessageDialog(null, "ERRO, ALBUM INVALIDO");
+							JOptionPane.showMessageDialog(null, "ERRO, CIDADE INVALIDA");
 						}
 					} else {
-						JOptionPane.showMessageDialog(null, "ERRO, ARTISTA INVALIDO");
+						JOptionPane.showMessageDialog(null, "ERRO, EMAIL INVALIDO");
 					}
 				} else {
-					JOptionPane.showMessageDialog(null, "ERRO, TITULO INVALIDO");
+					JOptionPane.showMessageDialog(null, "ERRO, NOME INVALIDO");
 				}
 			}
-		});
-		btnCadastrar.setBounds(305, 322, 97, 25);
-		contentPane.add(btnCadastrar);
 
+		}	
+
+		);
+
+		btnNewButton.setBounds(252, 362, 190, 29);
+		contentPane.add(btnNewButton);
+		
 		JButton btnRetornar = new JButton("Retornar");
+		btnRetornar.setFont(new Font("OCR A Extended", Font.PLAIN, 14));
 		btnRetornar.addActionListener(new ActionListener() {
-
-			@Override
 			public void actionPerformed(ActionEvent e) {
 				dispose();
-				TelaUsuario telaUsuario = new TelaUsuario(u);
-				telaUsuario.setVisible(true);
-				telaUsuario.setResizable(false);
-				telaUsuario.setLocationRelativeTo(null);
+				TelaLogin telaLogin = new TelaLogin();
+				telaLogin.setVisible(true);
+				telaLogin.setLocationRelativeTo(null);
+				telaLogin.setResizable(false);
 			}
 		});
-		btnRetornar.setBounds(80, 443, 97, 25);
+		btnRetornar.setBounds(140, 450, 108, 29);
 		contentPane.add(btnRetornar);
+				
+				JLabel lblserUsadoPara = new JLabel("(Ser\u00E1 usado para o login)");
+				lblserUsadoPara.setForeground(SystemColor.controlHighlight);
+				lblserUsadoPara.setFont(new Font("OCR A Extended", Font.BOLD, 15));
+				lblserUsadoPara.setBounds(450, 34, 266, 16);
+				contentPane.add(lblserUsadoPara);
+		
+				JLabel label = new JLabel(" ");
+				label.setIcon(new ImageIcon("Imagens\\JSPlayer 4.jpg"));
+				label.setBounds(0, 0, 844, 500);
+				contentPane.add(label);
 
-		JButton btnEndereco = new JButton("Endereco");
-		  btnEndereco.addActionListener(new ActionListener() {
-		   public void actionPerformed(ActionEvent e) {
-		    final JFileChooser jfc = new JFileChooser(new File("JSPMusicas\\"));
-		    
-		    FileFilter filter = new FileNameExtensionFilter("MP3 File","mp3");
-		    jfc.setFileFilter(filter);
-		    
-		    int retorno = jfc.showOpenDialog(null);
-		    if (retorno == JFileChooser.APPROVE_OPTION) {
-		     end = jfc.getSelectedFile().getAbsolutePath();
-		     end = end.substring(end.indexOf("JSPMusicas\\"));
-		     textEndereco.setText(end);
-		    }
-		   }
-		  });
-		  btnEndereco.setBounds(115, 226, 97, 25);
-		  contentPane.add(btnEndereco);
-		  
-		  
 	}
 }
